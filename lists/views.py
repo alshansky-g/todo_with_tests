90d3512520ce40f16dab4482237cd178a653c05d
alshansky-g <rgcs.rgcs@gmail.com>
2026-03-10 16:07:00 +0300

    add redirect after post and show all list elems in template

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpRequest
+
+from lists.models import Item
 
 
 def home_page(request: HttpRequest):
-    return render(
-        request,
-        "home.html",
-        {"new_item_text": request.POST.get("item_text", "")},
-    )
+    if request.method == "POST":
+        Item.objects.create(text=request.POST.get('item_text'))
+        return redirect('/')
+
+    return render(request, "home.html", {'items': Item.objects.all()})
