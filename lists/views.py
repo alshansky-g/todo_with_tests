871d0c67cfc3ca4cb531079e7416e7c242b95ff2
alshansky-g <rgcs.rgcs@gmail.com>
2026-03-10 03:24:56 +0300

    basic view now returns html

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-def home_page():
-    pass
+def home_page(request: HttpRequest):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
