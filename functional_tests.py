0086c00a1c062ab9a1bb1be636da842fb4d40a94
alshansky-g <rgcs.rgcs@gmail.com>
2026-03-10 13:00:18 +0300

    add options to test list element in testcase

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,13 @@
-from selenium import webdriver
+import time
+from selenium.webdriver.firefox.webdriver import WebDriver as Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.browser = webdriver.Firefox()
+        self.browser = Firefox()
 
     def tearDown(self) -> None:
         self.browser.quit()
@@ -13,7 +16,26 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+        self.fail('Закончить тест')
 
 
 if __name__ == '__main__':
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 
+
 def home_page(request: HttpRequest):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return HttpResponse("""
+                        <html><title>To-Do lists</title></html>
+                        """)
