da2a2d431ba0cd4d2772cfb6a57b0889f6331641
alshansky-g <rgcs.rgcs@gmail.com>
2026-03-11 23:29:32 +0300

    add collectstatic to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,14 @@ COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 COPY pyproject.toml uv.lock /src/
 WORKDIR /src
 
+ENV DJANGO_DEBUG_FALSE=1
+ENV DJANGO_SECRET_KEY=secret
+ENV DJANGO_ALLOWED_HOST=localhost
+
 RUN uv sync --frozen --no-dev
 
 COPY src /src
+
+RUN uv run python manage.py collectstatic --no-input
 
 CMD ["uv", "run", "gunicorn", "--bind", ":8888", "superlists.wsgi:application"]
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-enut040#%r24ev&h03sddr2!(e-h4eh_5z%na$_+liv37j(=n0'
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOST']]
 
 
 # Application definition
