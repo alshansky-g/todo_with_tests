09016779a3e16ddf39e0ec958170041da7996423
alshansky-g <rgcs.rgcs@gmail.com>
2026-03-14 12:42:14 +0300

    FT for duplicate item validation

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -27,3 +27,19 @@ class ItemValidationTest(FunctionalTest):
 
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('2: Приготовить чаю')
+
+    def test_cannot_add_duplicate_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Купить обувь')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить обувь')
+
+        self.get_item_input_box().send_keys('Купить обувь')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, '.invalid-feedback').text,
+                'Эта запись уже была добавлена ранее',
+            )
+        )
