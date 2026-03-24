471043568dc008a1330bc1a804c99ce7a093f55b
alshansky-g <rgcs.rgcs@gmail.com>
2026-03-25 02:31:55 +0300

    fix test_logged_in FT

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -44,6 +44,9 @@ class MyListsTest(FunctionalTest):
             )
         )
 
+        self.wait_for(
+            lambda: self.browser.find_element(By.LINK_TEXT, 'Reticulate splines')
+        )
         self.browser.find_element(By.LINK_TEXT, 'Reticulate splines').click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, first_list_url)
